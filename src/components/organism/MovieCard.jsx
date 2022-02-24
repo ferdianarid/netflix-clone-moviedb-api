a5d54f967c69389b00c07b1a5958bea6015c2596
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-02-24 19:11:01 +0700

    update moviecard

--- src/components/organism/MovieCard.jsx
+++ src/components/organism/MovieCard.jsx
@@ -5,10 +5,10 @@ import { StarIcon } from "@heroicons/react/solid"
 const MovieCard = ({ movieId, coverImage, movieTitle, votesAverage, popularity, releaseDate, altImage }) => {
        return (
               <React.Fragment>
-                     <div className="max-w-[250px] bg-[#0f0f18] text-white rounded-md p-3">
+                     <div className="max-w-[250px] bg-[#0f0f18] text-white p-3">
                             <div className="relative">
                                    <h3 className="text-gray-900 text-[8px] font-bold bg-yellow-300 absolute z-10 top-2 left-2 px-1 py-[1px] rounded-sm">{movieId}</h3>
-                                   <img className='h-[180px] transition-all object-cover w-full rounded-md' src={coverImage} alt={altImage} />
+                                   <img className='h-[180px] transition-all object-cover w-full' src={coverImage} alt={altImage} />
                             </div>
                             <div className="m-2 ">
                                    <p className="text-[10px] font-medium mb-4 mt-4 flex items-center"><span className='bg-yellow-300 flex items-center w-fit rounded-md p-[3px] gap-x-1 font-bold mr-2 pr-1 text-gray-800'><StarIcon className='w-3 h-3 text-gray-900 bg-yellow-300' />{votesAverage}</span>Vote average</p>
